79665459a9fb3420d652f3f6f6f85931ebbe45d3
Connor Horton <connor@Connors-MacBook-Pro.local>
2022-12-09 12:14:00 -0800

    add WGS bash scripts

---
+++ WGS_coverage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=fc_insertion1
+#SBATCH --partition=savio3
+#SBATCH --time=0-6
+
+ml samtools
+ml bwa
+
+mkdir stats
+
+bwa mem -t 32 templates_refs/chm13v2.0.fa.gz 01-TrimmedData/KCXZ0001A_R1_Trimmed_paired.fastq.gz 01-TrimmedData/KCXZ0001A_R2_Trimmed_paired.fastq.gz > 02-MappedData/KCXZ0001A_T2T.sam
+
+SIZE=$(samtools view -H 02-MappedData/KCXZ0001A_T2T.sam | grep -P '^@SQ' | cut -f 3 -d ':' | awk '{sum+=$1} END {print sum}')
+samtools depth -a 02-MappedData/KCXZ0001A_T2T.sam | awk -v var="$SIZE" '{sum+=$3} END {print sum/var}' > stats/KCXZ0001A_WGScoverage.txt
