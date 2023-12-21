52f7aa07e9782be429d71c6edbf203a55457e0e0
Connor Horton <connorhorton6@gmail.com>
2023-12-21 13:59:04 -0800

    bug fixes

--- WGS_coverage.sh
+++ WGS_coverage.sh
@@ -8,7 +8,7 @@ ml bwa
 
 mkdir stats
 
-bwa mem -t 32 templates_refs/chm13v2.0.fa.gz 01-TrimmedData/KCXZ0001A_R1_Trimmed_paired.fastq.gz 01-TrimmedData/KCXZ0001A_R2_Trimmed_paired.fastq.gz > 02-MappedData/KCXZ0001A_T2T.sam
+bwa mem -t 32 references/chm13v2.0.fa.gz 01-TrimmedData/KCXZ0001D_R1_Trimmed_paired.fastq.gz 01-TrimmedData/KCXZ0001D_R2_Trimmed_paired.fastq.gz > 02-MappedData/KCXZ0001D_T2T.sam
 
-SIZE=$(samtools view -H 02-MappedData/KCXZ0001A_T2T.sam | grep -P '^@SQ' | cut -f 3 -d ':' | awk '{sum+=$1} END {print sum}')
-samtools depth -a 02-MappedData/KCXZ0001A_T2T.sam | awk -v var="$SIZE" '{sum+=$3} END {print sum/var}' > stats/KCXZ0001A_WGScoverage.txt
+SIZE=$(samtools view -H 02-MappedData/KCXZ0001D_T2T.sam | grep -P '^@SQ' | cut -f 3 -d ':' | awk '{sum+=$1} END {print sum}')
+samtools depth -a 02-MappedData/KCXZ0001D_T2T.sam | awk -v var="$SIZE" '{sum+=$3} END {print sum/var}' > stats/KCXZ0001D_WGScoverage.txt
--- transgene_alignment.sh
+++ transgene_alignment.sh
@@ -8,30 +8,30 @@ ml bwa
 ml java
 
 # deduplicate reads
-bash bbmap/clumpify.sh in1=00-RawData/KCXZ0001A_S47_L004_R1_001_Trimmed.fastq.gz \
-in2=00-RawData/KCXZ0001A_S47_L004_R2_001_Trimmed.fastq.gz \
-out1=00-RawData/KCXZ0001A_R1.dedup.fastq.gz \
-out2=00-RawData/KCXZ0001A_R2.dedup.fastq.gz \
+bash bbmap/clumpify.sh in1=00-RawData/KCXZ0001D_S13_L004_R1_001.fastq.gz \
+in2=00-RawData/KCXZ0001D_S13_L004_R2_001.fastq.gz \
+out1=00-RawData/KCXZ0001D_R1.dedup.fastq.gz \
+out2=00-RawData/KCXZ0001D_R2.dedup.fastq.gz \
 dedupe=t optical=f
 
 mkdir 01-TrimmedData
 
 # trim reads
-java -jar Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 32 00-RawData/KCXZ0001A_R1.dedup.fastq.gz 00-RawData/KCXZ0001A_R2.dedup.fastq.gz \
-01-TrimmedData/KCXZ0001A_R1_Trimmed_paired.fastq.gz 01-TrimmedData/KCXZ0001A_R1_Trimmed_unpaired.fastq.gz \
-01-TrimmedData/KCXZ0001A_R2_Trimmed_paired.fastq.gz 01-TrimmedData/KCXZ0001A_R2_Trimmed_unpaired.fastq.gz \
+java -jar Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 32 00-RawData/KCXZ0001D_R1.dedup.fastq.gz 00-RawData/KCXZ0001D_R2.dedup.fastq.gz \
+01-TrimmedData/KCXZ0001D_R1_Trimmed_paired.fastq.gz 01-TrimmedData/KCXZ0001D_R1_Trimmed_unpaired.fastq.gz \
+01-TrimmedData/KCXZ0001D_R2_Trimmed_paired.fastq.gz 01-TrimmedData/KCXZ0001D_R2_Trimmed_unpaired.fastq.gz \
 SLIDINGWINDOW:6:30 LEADING:30 TRAILING:30 MINLEN:36
 
 mkdir 02-MappedData
 
 # align reads to transgene reference
-bwa mem -t 32 templates_refs/220416_template+rDNAflanks.fa 01-TrimmedData/KCXZ0001A_R1_Trimmed_paired.fastq.gz 01-TrimmedData/KCXZ0001A_R2_Trimmed_paired.fastq.gz > 02-MappedData/KCXZ0001A_transgeneflanks-pe.sam
+bwa mem -t 32 references/CBh_template+rDNAflanks.fa 01-TrimmedData/KCXZ0001D_R1_Trimmed_paired.fastq.gz 01-TrimmedData/KCXZ0001D_R2_Trimmed_paired.fastq.gz > 02-MappedData/KCXZ0001D_transgeneflanks-pe.sam
 
 # filter reads, keeping only pairs where one or both mapped to transgene
-samtools view -h -F 4 -f 8 02-MappedData/KCXZ0001A_transgeneflanks-pe.sam -o temp.sam
-samtools sort temp.sam -o 02-MappedData/KCXZ0001A_out1.sam
-samtools view -h -F 8 -f 4 02-MappedData/KCXZ0001A_transgeneflanks-pe.sam -o temp.sam
-samtools sort temp.sam -o 02-MappedData/KCXZ0001A_out2.sam
-samtools view -h -F 12 02-MappedData/KCXZ0001A_transgeneflanks-pe.sam -o temp.sam
-samtools sort temp.sam -o 02-MappedData/KCXZ0001A_out3.sam
-samtools merge -f -o 02-MappedData/KCXZ0001A_transgeneflanks_mappedmates.sam 02-MappedData/KCXZ0001A_out1.sam 02-MappedData/KCXZ0001A_out2.sam 02-MappedData/KCXZ0001A_out3.sam
+samtools view -h -F 4 -f 8 02-MappedData/KCXZ0001D_transgeneflanks-pe.sam -o temp.sam
+samtools sort temp.sam -o 02-MappedData/KCXZ0001D_out1.sam
+samtools view -h -F 8 -f 4 02-MappedData/KCXZ0001D_transgeneflanks-pe.sam -o temp.sam
+samtools sort temp.sam -o 02-MappedData/KCXZ0001D_out2.sam
+samtools view -h -F 12 02-MappedData/KCXZ0001D_transgeneflanks-pe.sam -o temp.sam
+samtools sort temp.sam -o 02-MappedData/KCXZ0001D_out3.sam
+samtools merge -f -o 02-MappedData/KCXZ0001D_transgeneflanks_mappedmates.sam 02-MappedData/KCXZ0001D_out1.sam 02-MappedData/KCXZ0001D_out2.sam 02-MappedData/KCXZ0001D_out3.sam
